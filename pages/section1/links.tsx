e9c59e0b580f7611bbcf76ab2e9fb6998cd5ef29
Nam JangHyun <smash009@naver.com>
2023-02-14 15:25:45 +0900

    Feat: link page 추가

---
+++ pages/section1/links.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Links() {
+  return (
+    <main>
+      <h1>Links</h1>
+      <Link href="/section1/getStaticProps">/getStaticProps</Link>
+    </main>
+  );
+}
