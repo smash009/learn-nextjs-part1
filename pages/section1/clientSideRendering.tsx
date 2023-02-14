b9c6e7bfadf8ee39b3fa24af20907176a4831676
Nam JangHyun <smash009@naver.com>
2023-02-14 17:09:42 +0900

    Feat: clientSideRendering page 추가

---
+++ pages/section1/clientSideRendering.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next';
+import { useEffect, useState } from 'react';
+
+const Example: NextPage = () => {
+  const [data, setData] = useState(0);
+
+  useEffect(() => {
+    const delayInSeconds = 2;
+
+    new Promise<number>((resolve) =>
+      setTimeout(() => resolve(Math.random()), delayInSeconds * 1000)
+    ).then((result) => setData(result));
+  }, []);
+
+  return (
+    <main>
+      <h1>client-side data fetching</h1>
+      <p>값: {data}</p>
+    </main>
+  );
+};
+
+export default Example;
